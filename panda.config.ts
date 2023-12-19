19103439058308a7d337cfa99e323ed5af359ebd
imeankenshin <omurakenshin@gmail.com>
2023-12-19 15:35:30 +0900

    Change color name

--- panda.config.ts
+++ panda.config.ts
@@ -15,34 +15,34 @@ export default defineConfig({
     extend: {
       tokens: {
         colors: {
-          ["pantone.50"]: {
+          "warmGray.50": {
             value: "#fafaf9"
           },
-          ["pantone.100"]: {
+          "warmGray.100": {
             value: "#f5f5f4"
           },
-          ["pantone.200"]: {
+          "warmGray.200": {
             value: "#e7e5e4"
           },
-          ["pantone.300"]: {
+          "warmGray.300": {
             value: "#d6d3d1"
           },
-          ["pantone.400"]: {
+          "warmGray.400": {
             value: "#a8a29e"
           },
-          ["pantone.500"]: {
+          "warmGray.500": {
             value: "#78716c"
           },
-          ["pantone.600"]: {
+          "warmGray.600": {
             value: "#57534e"
           },
-          ["pantone.700"]: {
+          "warmGray.700": {
             value: "#44403c"
           },
-          ["pantone.800"]: {
+          "warmGray.800": {
             value: "#292524"
           },
-          ["pantone.900"]: {
+          "warmGray.900": {
             value: "#1c1917"
           }
         },
